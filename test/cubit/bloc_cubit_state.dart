d1571fac3df46b7aecbb066822743f64dfa4a907
Bostigger <tricksteck@gmail.com>
2023-03-04 18:56:09 +0000

    added test

---
+++ test/cubit/bloc_cubit_state.dart
@@ -0,0 +1,18 @@
+import 'package:test/test.dart';
+import 'package:babing_cubit/cubit/bloc_cubit_state.dart';
+
+void main() {
+  group('CounterState', () {
+    test('should return true when comparing two identical CounterState objects', () {
+      final counterState1 = CounterState(initialValue: 0, wasIncremented: true);
+      final counterState2 = CounterState(initialValue: 0, wasIncremented: true);
+      expect(counterState1, equals(counterState2));
+    });
+
+    test('should return false when comparing two different CounterState objects', () {
+      final counterState1 = CounterState(initialValue: 0, wasIncremented: true);
+      final counterState2 = CounterState(initialValue: 1, wasIncremented: false);
+      expect(counterState1, isNot(equals(counterState2)));
+    });
+  });
+}
